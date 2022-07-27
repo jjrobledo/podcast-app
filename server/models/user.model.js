ca51c5c044f525e8630818df7ee2d084d47bb916
jesse-robledo <robledo.josej@gmail.com>
2022-07-26 21:23:54 -0600

    created mongoose model for users

---
+++ server/models/user.model.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  id: { type: String, required: true },
+  username: { type: String, required: true },
+  email: { type: String, required: true },
+  feedList: { type: Array },
+});
+
+const User = new nongoose.model("User", userSchema);
+
+// Dummy data
+
+const user1 = {
+  id: "df11d34",
+  username: "user31",
+  email: "user31@email.com",
+  feedList: [
+    "https://theblogmillionaire.libsyn.com/rss",
+    "https://feeds.simplecast.com/BqbsxVfO",
+  ],
+};
+
+module.exports = User;
