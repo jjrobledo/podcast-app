38045bed5514053474992ef00654c41617bcb46a
jrobledo <robledo.josej@gmail.com>
2022-07-31 20:51:07 -0600

    added react frontend with create-react-app

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home.pages";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <div>
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
