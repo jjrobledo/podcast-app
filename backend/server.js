42eedfa2ad5a71ba641eb76ecb602cda936aa1e0
jrobledo <robledo.josej@gmail.com>
2022-08-02 21:35:01 -0600

    heroku commit

--- backend/server.js
+++ backend/server.js
@@ -18,10 +18,10 @@ app.use("/api/user", userRoutes);
 const path = require("path");
 
 // Step 1:
-app.use(express.static(path.resolve(__dirname, "./client/build")));
+app.use(express.static(path.resolve(__dirname, "../frontend/build")));
 // Step 2:
 app.get("*", function (request, response) {
-  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+  response.sendFile(path.resolve(__dirname, "../frontend/build", "index.html"));
 });
 
 mongoose
