55d8bac68dd010b551cd2a40276fc7dc1aab7148
jrobledo <robledo.josej@gmail.com>
2022-08-02 23:39:48 -0600

    added auth requirement for frontend router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { useAuthContext } from "./hooks/useAuthContext.hook";
 import Layout from "./components/Layout.component";
 import Home from "./pages/Home.pages";
 import Signup from "./pages/Signup.page";
@@ -7,15 +8,33 @@ import Login from "./pages/Login.page";
 import Podcast from "./pages/Podcast.pages";
 
 function App() {
+  // get user from useAuthContext hook
+  const { user } = useAuthContext();
   return (
     <div className="App">
       <BrowserRouter>
         <Layout>
           <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/:id" element={<Podcast />} />
-            <Route path="/signup" element={<Signup />} />
-            <Route path="/Login" element={<Login />} />
+            {/* if user is not logged in send them to the login page */}
+            <Route
+              path="/"
+              element={user ? <Home /> : <Navigate to="/login" />}
+            />
+            {/* if user is not logged in send them to the login page */}
+            <Route
+              path="/:id"
+              element={user ? <Podcast /> : <Navigate to="/login" />}
+            />
+            {/* if user is logged in send them to the home page */}
+            <Route
+              path="/signup"
+              element={!user ? <Signup /> : <Navigate to="/" />}
+            />
+            {/* if user is not logged in send them to the home page */}
+            <Route
+              path="/Login"
+              element={!user ? <Login /> : <Navigate to="/" />}
+            />
           </Routes>
         </Layout>
       </BrowserRouter>
