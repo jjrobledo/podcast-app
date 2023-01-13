b909676da5d20e341d8d70ff6955c5771ae70044
Jesse Robledo <robledo.josej@gmail.com>
2023-01-12 21:18:24 -0700

    removed extra / in url

--- frontend/src/hooks/useLogin.hook.js
+++ frontend/src/hooks/useLogin.hook.js
@@ -5,7 +5,7 @@ export const useLogin = () => {
   const [error, setError] = useState(null);
   const [isLoading, setIsLoading] = useState(null);
   const { dispatch } = useAuthContext();
-  const baseURL = "https://podcast-app.onrender.com/";
+  const baseURL = "https://podcast-app.onrender.com";
 
   const login = async (email, password) => {
     setIsLoading(true);
