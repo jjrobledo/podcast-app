028436ddcdf1885aa31693a52f18ef634f0cf990
Jesse Robledo <robledo.josej@gmail.com>
2023-01-12 22:44:03 -0700

    updates for deploy

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+//app.use(cors());
 app.use(morgan("dev"));
 
 app.use("/api/podcasts", podcastRoutes);
