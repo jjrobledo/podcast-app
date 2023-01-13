8fa86a507cc994cc20678b4c9e3cdf12d31edd05
Jesse Robledo <robledo.josej@gmail.com>
2023-01-12 22:17:10 -0700

    removed heroku config

--- backend/server.js
+++ backend/server.js
@@ -17,14 +17,14 @@ app.use(morgan("dev"));
 app.use("/api/podcasts", podcastRoutes);
 app.use("/api/user", userRoutes);
 // Accessing the path module
-const path = require("path");
+//const path = require("path");
 
-// Step 1:
+/* // Step 1:
 app.use(express.static(path.resolve(__dirname, "../frontend/build")));
 // Step 2:
 app.get("*", function (request, response) {
   response.sendFile(path.resolve(__dirname, "../frontend/build", "index.html"));
-});
+}); */
 
 mongoose
   .connect(process.env.MONGO_URI)
