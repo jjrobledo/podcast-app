bfae28763c648804ce357409a42b8c9c3f938d4d
Jesse Robledo <robledo.josej@gmail.com>
2023-03-27 14:55:17 -0600

    fixed bad path names in module imports

--- backend/src/routes/api.js
+++ backend/src/routes/api.js
@@ -1,12 +1,12 @@
 
 const express = require("express");
 
-const userRoutes = require("./routes/users/user.routes");
-const podcastRoutes = require("./routes/podcasts/podcast.routes");
+const userRouter = require("./users/user.routes");
+const podcastRouter = require("./podcasts/podcast.routes");
 
 const api = express.Router();
 
-api.use("/users", userRoutes);
-api.use("/podcasts", podcastRoutes);
+api.use("/users", userRouter);
+api.use("/podcasts", podcastRouter);
 
 module.exports = api;
