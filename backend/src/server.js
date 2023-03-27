a91315eac71a56e5404eb2aa1401e4d1a59bff10
Jesse Robledo <robledo.josej@gmail.com>
2023-03-27 13:27:03 -0600

    updated server.js to use app.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,38 +4,18 @@ require("dotenv").config();
 
 const app = require("./app");
 
-const {mongoConnect} = require("./services/mongo");
+const { mongoConnect } = require("./services/mongo");
 
 const PORT = process.env.PORT || 8000;
 
 const server = http.createServer(app);
 
-const express = require("express");
-const mongoose = require("mongoose");
-const morgan = require("morgan");
-const podcastRoutes = require("./routes/podcasts/podcast.routes");
-const userRoutes = require("./routes/users/user.routes");
+async function startServer() {
+    await mongoConnect();
 
-const PORT = process.env.PORT || 5000;
-
-const app = express();
-
-app.use(express.json());
-//app.use(cors());
-app.use(morgan("dev"));
-
-app.use("/api/podcasts", podcastRoutes);
-app.use("/api/user", userRoutes);
-
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => {
-    app.listen(PORT, () => {
-      console.log(
-        `➡️ Connected to MongoDb 🖥️. Server listening on port ${PORT} 🎧`
-      );
+    server.listen(PORT, () => {
+        console.log(`Server listening on port ${PORT}`);
     });
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+}
+
+startServer();
