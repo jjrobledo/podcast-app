984a22245434c92ea975377266e7996222757b1c
Jesse Robledo <robledo.josej@gmail.com>
2023-03-26 20:41:40 -0600

    removed unused code

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,5 @@
 require("dotenv").config();
 const express = require("express");
-const cors = require("cors");
 const mongoose = require("mongoose");
 const morgan = require("morgan");
 const podcastRoutes = require("./routes/podcast.routes");
